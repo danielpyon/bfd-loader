b55aed5ac7c63f82603415920dc2038600ddea6d
Daniel Pyon <pyondohyun@gmail.com>
2024-04-08 23:52:19 -0700

    load_symbols_bfd

--- loader.cc
+++ loader.cc
@@ -1,12 +1,55 @@
-#define PACKAGE "loader"
-#define PACKAGE_VERSION "1.2.3"
-
-// GNU binutils 2.42
-
 #include <bfd.h>
 #include <string>
 #include <iostream>
 #include "loader.h"
+
+static int load_symbols_bfd(bfd* bfd_h, Binary* bin) {
+    asymbol** bfd_symtab = NULL;
+    long ret;
+
+    long n = bfd_get_symtab_upper_bound(bfd_h);
+    if (n < 0) {
+        fprintf(stderr, "failed to read symtab (%s)\n",
+        bfd_errmsg(bfd_get_error()));
+        goto fail;
+    } else if (n) {
+        bfd_symtab = (asymbol**)malloc(n);
+        if(!bfd_symtab) {
+            fprintf(stderr, "out of memory\n");
+            goto fail;
+        }
+
+        long nsyms = bfd_canonicalize_symtab(bfd_h, bfd_symtab);
+
+        if(nsyms < 0) {
+            fprintf(stderr, "failed to read symtab (%s)\n",
+            bfd_errmsg(bfd_get_error()));
+            goto fail;
+        }
+
+        Symbol* sym;
+        for(int i = 0; i < nsyms; i++) {
+            if(bfd_symtab[i]->flags & BSF_FUNCTION) {
+                bin->symbols.push_back(Symbol());
+                sym = &bin->symbols.back();
+                sym->type = Symbol::SYM_TYPE_FUNC;
+                sym->name = std::string(bfd_symtab[i]->name);
+                sym->addr = bfd_asymbol_value(bfd_symtab[i]);
+            }
+        }
+    }
+
+    ret = 0;
+    goto cleanup;
+
+fail:
+    ret = -1;
+
+cleanup:
+    if (bfd_symtab) free(bfd_symtab);
+
+    return ret;
+}
 
 static bfd* open_bfd(std::string &fname) {
     static int bfd_inited = 0;
@@ -31,7 +74,6 @@ static bfd* open_bfd(std::string &fname) {
     }
 
     bfd_set_error(bfd_error_no_error);
-
 
     if (bfd_get_flavour(bfd_h) == bfd_target_unknown_flavour) {
         fprintf(stderr, "unrecognized format for binary '%s' (%s)\n",
@@ -64,7 +106,6 @@ void unload_binary(Binary* bin) {
         if (sec->bytes) {
             free(sec->bytes);
         }
-
     }
 }
 
